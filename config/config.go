02b470e376b0f8282f000c1bf43e797e07b855ee
chrisis <chrisis.zann@gmail.com>
2025-01-15 19:30:30 +0200

    removed depricated comments

--- config/config.go
+++ config/config.go
@@ -28,22 +28,10 @@ var db *sql.DB
 
 func New(configFile string) *Application {
 	tempConfig := LoadConfig(configFile)
-	return GetInstanceLocal(tempConfig)
+	return GetInstance(tempConfig)
 }
 
-func GetInstance() *Application {
-
-	// HARDCODED DB
-	once.Do(func() {
-		instance = &Application{
-			Models: repository.New(db),
-			JwtKey: []byte("default_secret_key"),
-		}
-	})
-	return instance
-}
-
-func GetInstanceLocal(t tempConfig) *Application {
+func GetInstance(t tempConfig) *Application {
 
 	once.Do(func() {
 		instance = &Application{
@@ -53,58 +41,6 @@ func GetInstanceLocal(t tempConfig) *Application {
 	})
 	return instance
 }
-
-// func (a *Application) LoadConfig(configFile string) {
-
-// 	data, err := os.ReadFile(configFile)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-
-// 	var dat map[string]interface{}
-// 	json.Unmarshal(data, &dat)
-
-// 	jwtKey, ok := dat["jwt_key"].(string)
-// 	if !ok {
-// 		log.Fatalf("jwt_key is not a string")
-// 	}
-// 	a.JwtKey = []byte(jwtKey)
-
-// 	db_user, ok := dat["db_user"].(string)
-// 	if !ok {
-// 		log.Fatalf("db_user is not a string")
-// 	}
-
-// 	db_password, ok := dat["db_password"].(string)
-// 	if !ok {
-// 		log.Fatalf("db_password is not a string")
-// 	}
-
-// 	db_host, ok := dat["db_host"].(string)
-// 	if !ok {
-// 		log.Fatalf("db_host is not a string")
-// 	}
-
-// 	db_name, ok := dat["db_name"].(string)
-// 	if !ok {
-// 		log.Fatalf("db_name is not a string")
-// 	}
-
-// 	log.Println("db user: " + db_user)
-// 	log.Println("db pass: " + db_password)
-// 	log.Println("db host: " + db_host)
-// 	log.Println("db name: " + db_name)
-
-// 	a.SqlCfg = &mysql.Config{
-// 		User:   db_user,
-// 		Passwd: db_password,
-// 		Net:    "tcp",
-// 		Addr:   db_host,
-// 		DBName: db_name,
-// 	}
-// 	// Bad practise :)
-// 	// log.Println("loaded jwt key:", a.JwtKey)
-// }
 
 func LoadConfig(configFile string) tempConfig {
 	var temp tempConfig
